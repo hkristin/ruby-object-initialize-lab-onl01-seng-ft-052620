8be49edfa87cd3203ae50c28e9f13cdce4f670fa
Kristin Howard <hkristin@andrew.cmu.edu>
2020-06-04 22:02:10 +0000

    Automatically backed up by Learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -4,6 +4,18 @@ def intitialize(name, breed = "Mutt")
   @name = name
   @breed = breed
 end
-  
+
+def name
+  @name
+end
+def name=(name)
+@name = name
+end
+def breed 
+  @breed
+end
+def breed=(breed)
+@breed = breed
+end
 end
 
