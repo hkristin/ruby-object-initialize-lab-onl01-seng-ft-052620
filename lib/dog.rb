6bfb4832b02cc477ba4596bc4bdcb96bdad6b264
Kristin Howard <hkristin@andrew.cmu.edu>
2020-06-04 21:56:59 +0000

    Automatically backed up by Learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -3,7 +3,8 @@ class Dog
 def intitialize
 @name = name
 end
-
+def intitialize
+  @breed = breed
 def name
 @name
 end
@@ -12,4 +13,9 @@ def name=(name)
   @name = name
 end
 
+def breed=(breed)
+  @breed = breed
+def breed
+@breed
+end
 end
